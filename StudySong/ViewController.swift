368f1eb58e1d59d0c4c316f185a7c834aa1b5043
twatanabe88 <s1210147.oliva@gmail.com>
2016-07-02 23:59:07 +0900

    Initial Commit

---
+++ StudySong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StudySong
+//
+//  Created by 渡邊翼 on 2016/07/02.
+//  Copyright © 2016年 渡邊翼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
